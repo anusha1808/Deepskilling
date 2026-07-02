87bedc71fcb1d92fbae45bf7e52ce14c5ad63718
anusha1808 <anushagupta1808@gmail.com>
2026-07-03 02:09:21 +0530

    Create BookService.java

---
+++ Spring Core and Maven/Exercise 5: Configuring the Spring IoC Container/BookService.java
@@ -0,0 +1,16 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+    private BookRepository bookRepository;
+
+    public BookService(){
+        System.out.println("Book Service Created");
+    }
+
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+        System.out.println("Dependency injected");
+    }
+}
