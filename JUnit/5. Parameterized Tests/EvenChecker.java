9ee75cd07562a62e744ae07c68a8b5020b7585db
anusha1808 <anushagupta1808@gmail.com>
2026-07-04 14:12:17 +0530

    Create EvenChecker.java

---
+++ JUnit/5. Parameterized Tests/EvenChecker.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class EvenChecker {
+    public boolean isEven(int number){
+        if(number % 2 == 0){
+            return true;
+        }
+        return false;
+    }
+}
