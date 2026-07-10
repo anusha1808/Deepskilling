be4e2605c49825bc4fc227da15e359e6347b2145
anusha1808 <anushagupta1808@gmail.com>
2026-07-10 19:50:36 +0530

    Create Main.java

---
+++ Engineering Concepts/Design Patterns and Principles/Proxy Pattern/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Image image = new ProxyImage("Nature.jpg");
+
+        System.out.println("First Display:");
+        image.display();
+
+        System.out.println();
+
+        System.out.println("Second Display:");
+        image.display();
+    }
+}
