c30cb80bea16dd5634e28da04efca81ac002a4d5
anusha1808 <anushagupta1808@gmail.com>
2026-07-10 19:57:51 +0530

    Create WebApp.java

---
+++ Engineering Concepts/Design Patterns and Principles/Observer Pattern/WebApp.java
@@ -0,0 +1,8 @@
+public class WebApp implements Observer {
+
+    @Override
+    public void update(String stockName, double price) {
+        System.out.println("Web App: " + stockName +
+                " price updated to ₹" + price);
+    }
+}
