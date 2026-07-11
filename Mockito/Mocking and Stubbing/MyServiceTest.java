29e238cfe54422bb0731ada3bf61e1216db7ff08
anusha1808 <anushagupta1808@gmail.com>
2026-07-11 09:38:39 +0530

    Create MyServiceTest.java

---
+++ Mockito/Mocking and Stubbing/MyServiceTest.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+public class MyServiceTest {
+
+    @Test
+    void testExternalApi() {
+
+        // Create mock object
+        ExternalApi mockApi = mock(ExternalApi.class);
+
+        // Stub method
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        // Use mock object
+        MyService service = new MyService(mockApi);
+
+        String result = service.fetchData();
+
+        // Verify output
+        assertEquals("Mock Data", result);
+    }
+}
