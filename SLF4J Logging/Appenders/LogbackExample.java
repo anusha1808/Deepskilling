006bcc7c5625f2ac27c083cdf595fe31b2946811
anusha1808 <anushagupta1808@gmail.com>
2026-07-11 09:06:24 +0530

    Create LogbackExample.java

---
+++ SLF4J Logging/Appenders/LogbackExample.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogbackExample {
+
+    private static final Logger logger = LoggerFactory.getLogger(LogbackExample.class);
+
+    public static void main(String[] args) {
+
+        logger.debug("This is a DEBUG message");
+        logger.info("This is an INFO message");
+        logger.warn("This is a WARN message");
+        logger.error("This is an ERROR message");
+
+    }
+}
